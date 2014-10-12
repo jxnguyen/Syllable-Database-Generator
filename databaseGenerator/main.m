63434157e00d11f3099cea160b4046a8e8d0b89b
John Nguyen <johnxuannguyen@gmail.com>
2014-10-12 18:31:50 +0200

    Initial Commit

---
+++ databaseGenerator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  databaseGenerator
+//
+//  Created by John Nguyen on 12/10/2014.
+//  Copyright (c) 2014 John Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
